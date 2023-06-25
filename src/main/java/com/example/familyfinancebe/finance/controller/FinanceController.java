d5d3aec3359b201d0e08c493c2833c1eee8321fc
Kornelijus Semėnas <121442495+Ka0gaMi@users.noreply.github.com>
2023-06-25 14:27:36 +0300

    Changed CrossOrigin

--- src/main/java/com/example/familyfinancebe/finance/controller/FinanceController.java
+++ src/main/java/com/example/familyfinancebe/finance/controller/FinanceController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/finance")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://familyfinance.vercel.app")
 public class FinanceController {
 
     private final FinanceService financeService;
